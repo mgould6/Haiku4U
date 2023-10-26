3f37fff6f01a5597d0ca695694cf1f5bf4d41437
MichaelG <mjgould06@gmail.com>
2023-10-26 10:50:07 -0500

    push prior to testing

---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask, request, jsonify, render_template
+import pyodbc
+import hashlib
+import logging
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+# Update with your SQL Server credentials and database details
+connection_string = (
+    'DRIVER={SQL Server};'
+    'SERVER=MGOULD;'  # Update server_name
+    'DATABASE=HaikuDB;'  # Update database_name
+    'UID=UserID;'  # Update user
+    'PWD=PasswordHash'  # Update password
+)
+
+# Establish a database connection
+connection = pyodbc.connect(connection_string)
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Retrieve data from the request
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')  # Assuming plaintext, you should hash this before querying
+
+    # Hash the password (using SHA-256 for this example)
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    # Your SQL query to check credentials. Update table_name and column names
+    query = f"""
+        SELECT * FROM Table_1 
+        WHERE UserName = '{username}' AND PasswordHash = '{hashed_password}'
+    """
+    
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchone()
+    
+    # Check if a result was returned
+    if result:
+        return jsonify(status="success", message="Login successful"), 200
+    else:
+        return jsonify(status="failure", message="Invalid credentials"), 401
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        # Retrieve data from the request
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')  # Assuming plaintext, you should hash this before storing
+
+        # Hash the password (using SHA-256 for this example)
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        # Your SQL query to check for duplicate usernames or emails.
+        check_query = f"""
+            SELECT * FROM Table_1
+            WHERE UserName = '{username}' OR Email = '{email}'
+        """
+
+        cursor = connection.cursor()
+        cursor.execute(check_query)
+        result = cursor.fetchone()
+
+        # Check if a result was returned indicating a duplicate username or email
+        if result:
+            return jsonify(status="failure", message="Username or email already exists"), 400
+
+        # Your SQL query to insert the new user data. Update table_name and column names
+        insert_query = f"""
+            INSERT INTO Table_1 (UserName, Email, PasswordHash)
+            VALUES ('{username}', '{email}', '{hashed_password}')
+        """
+        
+        cursor.execute(insert_query)
+        connection.commit()  # Commit the transaction
+
+        return jsonify(status="success", message="Signup successful"), 201
+    except Exception as e:
+        app.logger.error(f"An error occurred: {e}")
+        return "An error occurred", 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    # Run the Flask app on localhost, port 5000. Update if necessary
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,2 +1,123 @@
-import sys
-print(sys.executable)
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import re
+from email_validator import validate_email, EmailNotValidError
+
+# Load the login screen kv file at the module level
+Builder.load_file('login_screen.kv')
+Builder.load_file('sign_up_screen.kv')
+
+# Define each screen as a class
+class LoginScreen(Screen):
+    pass
+
+class SignUpScreen(Screen):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+class CreateHaikuScreen(Screen):
+    pass
+
+class ProfileScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+class ExploreScreen(Screen):
+    pass
+
+class NotificationsScreen(Screen):
+    pass
+
+class MainApp(App):
+    def validate_email(self, email):
+        try:
+            v = validate_email(email)
+            print("The email address is valid.")
+            return True
+        except EmailNotValidError as e:
+            print(str(e))
+            return False
+    
+    def username_filter(self, value, undo_operation):
+        if 3 <= len(value) <= 30 and re.match('^[a-zA-Z0-9_-]*$', value):
+            return value
+        return undo_operation
+
+    def password_filter(self, value, undo_operation):
+        if len(value) < 8:
+            return undo_operation
+        has_upper = any(c.isupper() for c in value)
+        has_lower = any(c.islower() for c in value)
+        has_digit = any(c.isdigit() for c in value)
+        has_special = any(not c.isalnum() for c in value)
+        if has_upper and has_lower and has_digit and has_special:
+            return value
+        return undo_operation
+    
+    def build(self):
+        # Create a ScreenManager instance
+        self.sm = ScreenManager()
+
+        # Add each screen to the ScreenManager
+        self.sm.add_widget(LoginScreen(name='login'))
+        self.sm.add_widget(SignUpScreen(name='signup'))
+        self.sm.add_widget(HomeScreen(name='home'))
+        self.sm.add_widget(CreateHaikuScreen(name='create_haiku'))
+        self.sm.add_widget(ProfileScreen(name='profile'))
+        self.sm.add_widget(SettingsScreen(name='settings'))
+        self.sm.add_widget(ExploreScreen(name='explore'))
+        self.sm.add_widget(NotificationsScreen(name='notifications'))
+
+        
+
+        return self.sm  # Return the ScreenManager as the root widget
+    
+    def change_screen(self, screen_name):
+        # Change the current screen of the ScreenManager
+        self.sm.current = screen_name
+
+    def login(self, *args):
+        pass  # Implement login logic
+
+    def show_signup(self, *args):
+        self.change_screen('signup')  # Navigate to Sign Up screen
+
+    def show_forgot_password(self, *args):
+        pass  # Navigate to Forgot Password screen
+
+    def sign_up(self, *args):
+        # Get the user input
+        username = self.sm.get_screen('signup').ids.username_input.text
+        email = self.sm.get_screen('signup').ids.email_input.text
+        password = self.sm.get_screen('signup').ids.password_input.text
+
+        # TODO: You might want to call validate_email here if you haven't validated the email earlier
+        if not self.validate_email(email):
+            print("Invalid email")
+            return
+        
+        # Call the filter functions to validate the input
+        if self.username_filter(username, None) is None:
+            print("Invalid username")
+            return
+        if self.password_filter(password, None) is None:
+            print("Invalid password")
+            return
+
+        # TODO: Implement sign-up logic here.
+        # For now, we'll just print the input values.
+        print(f'Username: {username}, Email: {email}, Password: {password}')
+
+        # After sign-up, you might want to navigate to the Home screen
+        self.change_screen('home')
+
+
+    
+if __name__ == "__main__":
+    MainApp().run()
